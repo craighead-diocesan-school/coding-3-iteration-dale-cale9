2f5a7eb434a167ed1d5271aa27e8b76a5f880f4d
dale-cale9 <145309461+dale-cale9@users.noreply.github.com>
2024-06-24 09:32:16 +1200

    mn

--- code/1-lift-off.js
+++ code/1-lift-off.js
@@ -4,16 +4,29 @@
 
 function liftOff() {
   let count = 10
- 
+
   while (count > 0) {
     alert(count + '...')
     count = count - 1
   }
-   
+
   alert('Lift Off!')
 }
 
 function make1() {
-  // copy and paste your code here when you get to the Make step
-  
+
+
+  let max = prompt('Enter a maximum number')
+  let increment = prompt('Enter a increment')
+  let count = 0
+  max = Number(max)
+  increment = Number(increment)
+  while (count <= max) {
+    alert(count)
+    leftover = max - count
+    count = count + increment
+
+  }
+  alert('done with ' + leftover + ' leftover?')
+
 }
--- code/2-attendees.js
+++ code/2-attendees.js
@@ -3,21 +3,36 @@
 // ####################################
 
 function attendees() {
+  let amount = prompt('How many attendees are there?')
+  alert('What are their names?')
   let count = 0
-  let numOfAttendees = 5
+  let numOfAttendees = amount
   let attendeeList = ''
-  
+
   while (count < numOfAttendees) {
     let attendee = prompt()
-    attendeeList = attendeeList + attendee
-    attendeeList = attendeeList + ', '
+    attendeeList = attendeeList + attendee + ', '
     count = count + 1
   }
-  
-  alert(attendeeList) 
+
+  alert(attendeeList)
 }
 
 function make2() {
-  // copy and paste your code here when you get to the Make step
-  
+
+  let numOfNumbers = prompt('How many numbers do you want to add?')
+
+  let count = 0
+  let numOfnumbers = amount
+  let numberList = ''
+
+
+  while (count < numOfnumbers) {
+    let newNum = prompt('name a number')
+    newNum = Number(newNum)
+    numberList = numberList + newNum
+    numberList = Number(nmberList)
+    count = count + 1
+  }
+  alert(numberList)
 }
